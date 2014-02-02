199529816899f41a49cfd7029049a69d6ad51143
Bryant Hughes <bryant@thegoodlab.com>
2014-02-02 10:55:05 -0600

    updating ish

--- config/converters/convertToEE.sh
+++ config/converters/convertToEE.sh
@@ -6,20 +6,20 @@ git clone git@codebasehq.com:thegoodlab/internal/ee-template.git TEMP
 # Moving everything we need from the EE repo
 mv ./TEMP/config/bootstrap.php ./config/bootstrap.php
 mv ./TEMP/config/config_local.php ./config/config_local.php
-mv -r ./TEMP/httpdocs ./httpdocs
-mv -r ./TEMP/system ./system
-mv -r ./TEMP/templates ./templates
-mv -r ./TEMP/third_party ./third_party
+mv ./TEMP/httpdocs ./httpdocs
+mv ./TEMP/system ./system
+mv ./TEMP/templates ./templates
+mv ./TEMP/third_party ./third_party
 mv ./TEMP/Boxfile ./Boxfile
 
 cat ./TEMP/README.md >> ./README.md
 
 # Removing the EE repo
-rm -r ./TEMP
+rm -r -f ./TEMP
 
 # Moving our assets dir
-mv -r ./assets ./httpdocs/assets
+mv ./assets ./httpdocs/assets
 
 # Replaceing paths in some configuration files
-sed -i 's/\/assets/\/httpdocs\/assets/g' ./.gitignore
-sed -i 's/assets/httpdocs\/assets/g' ./.Gruntfile.js
+sed 's/\/assets/\/httpdocs\/assets/g' .gitignore
+sed 's/assets/httpdocs\/assets/g' .Gruntfile.js
