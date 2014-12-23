689990b2e21995da7e4fd1c4af7406286b56a235
Bryant Hughes <bryant@authenticff.com>
2014-12-22 18:48:41 -0600

    updating the craft converter

--- config/converters/convertToCraft.sh
+++ config/converters/convertToCraft.sh
@@ -13,8 +13,15 @@ mv ./TEMP/craft ./craft
 # Moving our assets dir back
 mv ./TEMP/assets ./public/assets
 
+mv ./httpdocs/lib ./public/lib
+mv ./httpdocs/index.html ./public/index.html
+
 # Combine README's
 cat ./TEMP/readme.txt >> ./README.md
 
 # Removing the EE repo
 rm -r -f ./TEMP
+rm -r -f ./httpdocs
+
+sed -i 's/httpdocs/public/g' ./Gruntfile.js
+sed -i 's/httpdocs/public/g' ./.git/hooks/pre-commit
